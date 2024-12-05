cddb61fad4d4eb3dbf1be5993414f015bcbe92e7
James Gabriel <jamesgabriel8917@gmail.com>
2024-12-05 16:17:03 -0300

    Creation of the project

---
+++ ChefDelivery/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by James on 05/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
