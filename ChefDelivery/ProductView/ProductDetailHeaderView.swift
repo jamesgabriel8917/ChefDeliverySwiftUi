97040489f1f2b09fcd66cac35a5be2d0275da590
James Gabriel <jamesgabriel8917@gmail.com>
2024-12-16 16:04:56 -0300

    Concusao das aulas dos conceitos basicos da longuagem

---
+++ ChefDelivery/ProductView/ProductDetailHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailHeaderView.swift
+//  ChefDelivery
+//
+//  Created by James on 16/12/24.
+//
+
+import SwiftUI
+
+struct ProductDetailHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailHeaderView()
+}
